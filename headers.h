723fe5dde301c3b7b01600e4a54016321850b8bd
poorvaj-a <82881569+poorvaj-a@users.noreply.github.com>
2022-07-05 12:06:50 +0530

    Create headers.h

---
+++ headers.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <pwd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <fcntl.h>
